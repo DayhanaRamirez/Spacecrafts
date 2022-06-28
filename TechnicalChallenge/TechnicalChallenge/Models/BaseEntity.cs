8eefbce8155718c5075b94919c7a37d1ae68b22b
Dayhana Ramirez <dayhanaramirez45@gmail.com>
2022-06-27 19:59:16 -0500

    Changed unit tests.

--- TechnicalChallenge/TechnicalChallenge/Models/BaseEntity.cs
+++ TechnicalChallenge/TechnicalChallenge/Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿namespace TechnicalChallenge.Models
 {
+    //Base class for all the domain entities
     public abstract class BaseEntity
     {
         public int Id { get; set; }
--- TechnicalChallenge/TechnicalChallengeTest/MethodTest/MethodControllerTest.cs
+++ TechnicalChallenge/TechnicalChallengeTest/MethodTest/MethodControllerTest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TechnicalChallenge.Controllers;
 using TechnicalChallenge.Interfaces;
+using TechnicalChallenge.Models;
 
 namespace TechnicalChallengeTest.MethodTest
 {
@@ -20,42 +21,60 @@ namespace TechnicalChallengeTest.MethodTest
             _methodsController = new MethodsController(_mockMethodsServices.Object);
         }
 
+        //Testing abstract behaviour of objects
         [Fact]
         public void Recupera_Astronauta_Correctamente()
         {
+            //Arrange
             var txt = "I'm Daniel. I am an astronaut, a person trained, equipped, and deployed by a human spaceflight program to serve as a commander or crew member aboard a spacecraft."
                 + "I work 10 hours per day.";
+            Astronaut mockAstronaut = new()
+            {
+                Name = "Daniel"
+            };
+            var txtExpected = mockAstronaut.SelfDescription() + mockAstronaut.ShiftLength();
 
             Task<string> res = Task<string>.Factory.StartNew(() =>
             {
                 return txt;
             });
 
-            var _methodsController = new MethodsController(_mockMethodsServices.Object);
-
             _mockMethodsServices.Setup(x => x.GetAstronaut(1)).Returns(res);
+
+            //Act
             var astronauta = _methodsController.GetAstronaut(1);
-            Assert.Equal(txt, astronauta.Result);
+
+            //Assert
+            Assert.Equal(txtExpected, astronauta.Result);
         }
 
+        //Testing abstract behaviour of objects
         [Fact]
         public void Recupera_AstronomicalObject_Correctamente()
         {
-
+            //Arrange
             var txt = "I'm Moon. I am an astronomical object, a naturally occurring physical entity, association, or structure that exists in the observable universe."
                 + "I work the same hours as my space vehicle.";
 
+            AstronomicalObject mockAstronomicalObject = new()
+            {
+                Name = "Moon"
+            };
+
+            var txtExpected = mockAstronomicalObject.SelfDescription() + mockAstronomicalObject.ShiftLength();
+            
             Task<string> res = Task<string>.Factory.StartNew(() =>
             {
                 return txt;
             });
 
-            var _methodsController = new MethodsController(_mockMethodsServices.Object);
-
             _mockMethodsServices.Setup(x => x.GetAstronomicalObject(1)).Returns(res);
-            var astronomicalObject = _methodsController.GetAstronomicalObject(1);
-            Assert.Equal(txt, astronomicalObject.Result);
 
+            //Act
+            var astronomicalObject = _methodsController.GetAstronomicalObject(1);
+
+            //Assert
+            Assert.Equal(txtExpected, astronomicalObject.Result);
         }
     }
 }
